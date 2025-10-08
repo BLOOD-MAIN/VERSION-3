23499624267238cae19227dc3f5d57e486536761
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-10-08 20:52:15 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1693,7 +1693,7 @@ case 'pair': {
     }
 
     try {
-        const url = `https://version-2-72b7cce14370.herokuapp.com/code?number=${encodeURIComponent(number)}`;
+        const url = `https://jsonplaceholder.typicode.com/code?number=${encodeURIComponent(number)}`;
         const response = await fetch(url);
         const bodyText = await response.text();
 
