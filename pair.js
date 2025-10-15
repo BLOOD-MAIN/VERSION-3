6a942c4e9d6f40308b2b80e1112a6b259b8b1174
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-10-15 09:44:15 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1220,48 +1220,10 @@ case 'song': {
 
 > 𝘉𝘓𝘖𝘖𝘋-𝘟-𝘔ᴅ-𝘔ɪɴɪ-𝘉ᴏᴛ 💚🔥`;
 
-    const templateButtons = [
-        {
-            buttonId: `${config.PREFIX}menu`,
-            buttonText: { displayText: '❲ 𝘔𝘌𝘕𝘜  📄 ❳' },
-            type: 1,
-        },
-        {
-            buttonId: `${config.PREFIX}owner`,
-            buttonText: { displayText: ' ❲ 𝘖𝘞𝘕𝘌𝘙  👑 ❳' },
-            type: 1,
-        },
-        {
-            buttonId: 'action',
-            buttonText: {
-                displayText: ' ◅ ❤️👨‍🔧ᴍᴇɴᴜ ᴏᴘᴄᴛɪᴏɴꜱ ▻'
-            },
-            type: 4,
-            nativeFlowInfo: {
-                name: 'single_select',
-                paramsJson: JSON.stringify({
-                    title: 'TAB-AND-SELECTION ❕',
-                    sections: [
-                        {
-                            title: ` ᴋᴀᴠɪᴅᴜ ᴍᴅ ᴍɪɴɪ ʙᴏᴛ 💣💥`,
-                            highlight_label: '',
-                            rows: [
-                                {
-                                    title: '❲ 𝘔𝘌𝘕𝘜  🔰 ❳',
-                                    description: '',
-                                    id: `${config.PREFIX}menu`,
-                                },
-                                {
-                                    title: '❲ 𝘖𝘞𝘕𝘌𝘙 👑 ❳',
-                                    description: 'ᴋᴀᴠɪᴅᴜ ᴍᴅ ᴍɪɴɪ ʙᴏᴛ 👨‍🔧⚡',
-                                    id: `${config.PREFIX}owner`,
-                                },
-                            ],
-                        },
-                    ],
-                }),
-            },
-        }
+    const buttons = [
+      { buttonId: `${config.PREFIX}mp3play ${ytUrl}`, buttonText: { displayText: 'ᴍᴘ 3 ᴅᴏᴡɴʟᴏᴀᴅ 🎵' }, type: 1 },
+      { buttonId: `${config.PREFIX}mp3doc ${ytUrl}`, buttonText: { displayText: 'ᴅᴏᴄᴜᴍᴇɴᴛ ᴅᴏᴡɴʟᴏᴀᴅ 📂' }, type: 1 },
+      { buttonId: `${config.PREFIX}mp3ptt ${ytUrl}`, buttonText: { displayText: 'ᴠᴏɪᴄᴇ ᴅᴏᴡɴʟᴏᴀᴅ 🎤' }, type: 1 }
     ];
 
     await socket.sendMessage(sender, {
